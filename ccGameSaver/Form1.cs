e8741f481998dd4729b0f9f0897a17d0fe4233f6
Jalius <jaliuswelch2@gmail.com>
2020-12-16 15:32:45 -0800

    Add project files.

---
+++ ccGameSaver/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ccGameSaver
+{
+    public partial class Form1 : Form
+    {
+        int user_custom_autosave_interval = 0;
+        GameSaverRestorer gameSaverRestorer = new GameSaverRestorer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void SetAutoSaveInterval(int interval)
+        {
+            if (interval > 0)
+            {
+                timer1.Interval = 60 * 1000 * interval; //ms to minutes conversion
+                timer1.Start();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+        private void StoreUserCustomAutosaveInterval(string user_input)
+        {
+            int user_input_number = 0;
+            if (Int32.TryParse(user_input, out user_input_number))
+            {
+                user_custom_autosave_interval = user_input_number;
+            }
+        }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            SetAutoSaveInterval(2);
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            SetAutoSaveInterval(5);
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            SetAutoSaveInterval(10);
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            SetAutoSaveInterval(user_custom_autosave_interval);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            StoreUserCustomAutosaveInterval(textBox1.Text);
+            if(radioButton4.Checked)
+            {
+                SetAutoSaveInterval(user_custom_autosave_interval);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gameSaverRestorer.SaveGame("manual");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem != null)
+            {
+                gameSaverRestorer.BackupThenRestoreGame(comboBox1.SelectedItem.ToString());
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool isRunning = Process.GetProcessesByName("CannibalCrossing").Any();
+            if (!isRunning)
+            {
+                label4.Visible = true;
+                label4.Text = "skipped autosave, CannibalCrossing.exe not open";
+                return;
+            }
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            string save_name = gameSaverRestorer.SaveGame("autosave");
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+            double truncated = Math.Truncate(ts.TotalSeconds * 100) / 100;
+
+            label4.Visible = true;
+            label4.Text = save_name + " created in " + truncated + " s";
+            return;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval =  60 * 1000 * 5;//5 minutes
+            timer1.Start();
+        }
+
+        private void comboBox1_Enter(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            var local_save_names = gameSaverRestorer.GetLocalSaveNames();
+            foreach (var save in local_save_names)
+            {
+                comboBox1.Items.Add(save);
+            }
+
+        }
+
+        private void StorePreserveAutosaveCount(string user_input)
+        {
+            int user_input_number = 0;
+            if (Int32.TryParse(user_input, out user_input_number))
+            {
+                gameSaverRestorer.preserve_autosave_count = user_input_number;
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            StorePreserveAutosaveCount(textBox2.Text);
+        }
+    }
+}
---
+++ ccGameSaver/GameSaverRestorer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ccGameSaver
+{
+    public class GameSaverRestorer
+    {
+        static readonly string AppDataPath = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).ToString();
+
+        public readonly string saveGamePath = Path.Combine(
+            AppDataPath,
+            "LocalLow\\AwesomeLand\\CannibalCrossing"
+            );
+        public readonly string saveGameBackupsPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+            "ccGameSaver"
+            );
+        bool loaded_local_saves = false;
+        List<string> my_local_saves = new List<string>();
+        public int preserve_autosave_count { get; set; } = 5;
+        public GameSaverRestorer()
+        {
+        }
+        private void ClearPrevAutosaves(int preserve_count = 5)
+        {
+            if (!loaded_local_saves)
+            {
+                LoadLocalSaves();
+                loaded_local_saves = true;
+            }
+            var my_local_autosaves = my_local_saves
+                .Where(save => save.Substring(0, save.LastIndexOf("-")) == "autosave")
+                .OrderByDescending(save => save.Substring(save.LastIndexOf("-") + 1));
+            var saves_to_clear = my_local_autosaves.Skip(preserve_count).Take(my_local_autosaves.Count() - preserve_count);
+            var preserved_local_autosaves = my_local_autosaves.Take(preserve_count);
+            foreach (var save in saves_to_clear)
+            {
+                var save_full_path = Path.Combine(saveGameBackupsPath, save);
+                try
+                {
+                    Directory.Delete(save_full_path, true);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    break;
+                }
+                finally
+                {
+                    my_local_saves.Remove(save);
+                }
+            }
+        }
+        public string SaveGame(string save_name_prefix)
+        {
+            string save_name = save_name_prefix + "-" + DateTime.Now.ToString("yyyyMMddTHHmmss");
+            string save_path = Path.Combine(saveGameBackupsPath, save_name);
+            try
+            {
+                Microsoft.VisualBasic.FileIO.FileSystem.CopyDirectory(saveGamePath, save_path);
+            }
+            catch (DirectoryNotFoundException)
+            {
+
+                return "No Save Found";
+            }
+            my_local_saves.Add(save_name);
+            ClearPrevAutosaves(preserve_autosave_count);
+            return save_name;
+        }
+        private string RestoreGame(string save_name)
+        {
+            bool isRunning = Process.GetProcessesByName("CannibalCrossing").Any();
+            if(isRunning)
+            {
+                MessageBox.Show("Close Cannibal Crossing First");
+                return save_name;
+            }
+            try
+            {
+                Directory.Delete(saveGamePath, true);
+            }
+            catch (DirectoryNotFoundException)
+            {
+            }
+            string save_path = Path.Combine(saveGameBackupsPath, save_name);
+            Microsoft.VisualBasic.FileIO.FileSystem.CopyDirectory(save_path, saveGamePath);
+            return save_name;
+        }
+        public void BackupThenRestoreGame(string save_restore_folder_name)
+        {
+            //backup current save first
+            SaveGame("prerestore");
+            //restore a save
+            RestoreGame(save_restore_folder_name);
+        }
+        private void LoadLocalSaves()
+        {
+            var di = new DirectoryInfo(saveGameBackupsPath);
+            try
+            {
+               my_local_saves.AddRange(
+                     di.GetDirectories()
+                     .Select(dir_info => dir_info.Name)
+                     .ToList());
+            }
+            catch (DirectoryNotFoundException) // no local saves exist
+            {
+            }
+        }
+        public List<string> GetLocalSaveNames()
+        {
+            if(!loaded_local_saves)
+            {
+                LoadLocalSaves();
+                loaded_local_saves = true;
+            }
+            my_local_saves = my_local_saves.OrderByDescending
+                (
+                f_name => f_name.Substring(f_name.LastIndexOf('-') + 1)
+                ).ToList();
+            return my_local_saves;
+        }
+    }
+}
